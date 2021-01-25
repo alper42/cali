bdada423c95963872d02f3c3b50e49a5f272385c
alper42 <77581015+alper42@users.noreply.github.com>
2021-01-25 20:44:45 +0100

    my first commit

---
+++ script.js
@@ -0,0 +1,14 @@
+document.getElementById("fontInsta").onclick = function (){
+    
+    window.location = "https://www.instagram.com/"
+}
+
+document.getElementById("fontFace").onclick = function (){
+    
+    window.location = "https://www.facebook.com/"
+}
+
+document.getElementById("fontTwitter").onclick = function (){
+    
+    window.location = "https://www.twitter.com/"
+}
